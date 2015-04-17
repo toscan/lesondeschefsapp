d71c548bde5297bbd80a8f2219fd2311d135cfc6
unknown <alexisleboulch@gmail.com>
2015-04-17 13:31:27 +0200

    jukebox(css, html, js) playing(html, js)

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
     resize_cube();
     init_cube();
     swipe_cube();
+    touch_menu_jukebox();
 }, false);
 
 var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
-    .when('/home', { templateUrl: 'partials/home.html' })
+    .when('/home', { templateUrl: 'partials/jukebox.html' })
     .when('/jukebox', { templateUrl: 'partials/jukebox.html' })
     .when('/playing', { templateUrl: 'partials/playing.html' })
     .when('/account', { templateUrl: 'partials/account.html' })
---
+++ js/jukebox.js
@@ -0,0 +1,10 @@
+﻿function touch_menu_jukebox() {
+    $('#content').on("tap", function () {
+        if ($('#options').hasClass('position_init')) {
+            $('#options').removeClass('position_init').addClass('position_finale');
+        }
+        else {
+            $('#options').removeClass('position_finale').addClass('position_init');
+        }
+    });
+}
